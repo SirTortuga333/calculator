7fccb2327e18966aeb909d73ad384c74162e45d7
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2025-01-05 23:40:12 -0300

    Made basic template for calculator, need to fix CSS Made basic functions in js, needs testing after html fix

---
+++ script.js
@@ -0,0 +1,39 @@
+// Basic Calculator Functions
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+function operate(operator, a, b){
+    switch (operator) {
+        case '+':
+            return add(a,b)            
+            break;
+
+        case '-':
+            return subtract(a,b)            
+            break;
+    
+        case '*':
+            return multiply(a,b)            
+            break;
+
+        case '/':
+            return divide(a,b)            
+            break;
+
+        default:
+            console.log('Not a valid operator!')
+            break;
+    }
+}
+
+let num1, num2, operator;
