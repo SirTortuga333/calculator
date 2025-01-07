364b59f193abc3558b6958e98fb93ef9b6dbd41f
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2025-01-06 23:55:28 -0300

    IDK what change I made in Index.html Added eventListeners to buttons, created function evalString Calculator is working with simple operations

--- script.js
+++ script.js
@@ -32,8 +32,56 @@ function operate(operator, a, b){
 
         default:
             console.log('Not a valid operator!')
+            return 'ERROR'
             break;
     }
 }
 
+// Eval Function
+function evalString(string){
+    
+    const operation = [...string];
+
+    operatorIndex = operation.findIndex(item => operators.includes(item))
+    num1 = operation.slice(0, operatorIndex).join('')
+    num2 = operation.slice(operatorIndex+1).join('')
+    operator = operation.slice(operatorIndex, operatorIndex+1).toString()
+
+    console.log(operator)
+
+    return operate(operator, parseInt(num1), parseInt(num2));
+}
+
+
+const numbers = '0123456789';
+const operators = '+-*/';
 let num1, num2, operator;
+
+// Add event listeners for buttons
+    // the function needs to write the button.textContent to .result secuentially
+    // and save the numbers and operator to respective variable for reading
+        // maybe save everything as string and ... the string to read it
+
+const buttons = [...document.querySelectorAll('button')];
+const result = document.querySelector('.result');
+
+buttons.forEach(element => {
+
+    if(element.classList.contains('equal')){
+        element.addEventListener('click', (Element) => {
+            if(result.textContent !== ''){
+                result.textContent = evalString(result.textContent);
+            }
+        })
+    }else{
+        element.addEventListener('click', (Element) => {
+            
+            if(result.textContent === '' && Element.target.classList.contains('operator')){
+                result.textContent = '0' + Element.target.textContent
+            }else{
+                result.textContent += Element.target.textContent
+            }
+            return
+        })
+    }
+});
